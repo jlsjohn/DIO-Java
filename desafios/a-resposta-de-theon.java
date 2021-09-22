6748662b9064211d7cd08c336edd6ed635e9b1e8
Jonathas <jlsjohn@users.noreply.github.com>
2021-09-22 17:07:30 -0300

    Entrega do segundo desafio da DIO.

---
+++ desafios/a-resposta-de-theon.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+	
+    public static void main(String[] args) throws IOException {
+		Scanner leitor = new Scanner(System.in);
+		int N = leitor.nextInt();
+		int T = 0;
+		int menor = 10000, posMenor = 0;
+		for (int i = 1; i <= N; ++i) {
+			T = leitor.nextInt();
+			
+			if (T < menor) {
+			  menor = T;
+			  posMenor = i;
+			}
+		}
+		System.out.println(posMenor);
+    }
+	
+}
